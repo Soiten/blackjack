6d95ebc4a8e4b19c38c5e3c356ebd7535b19d4f8
Renan Araujo Polainas <re.araujo.po@gmail.com>
2025-08-18 18:54:55 -0300

    testando protocolo startGame

--- test.js
+++ test.js
@@ -4,13 +4,21 @@ const socket = new WebSocket("ws://localhost:3000");
 
 socket.on("open", () => {
   console.log("Conectado ao servidor!");
-  socket.send(JSON.stringify({ type: "createPlayer", data: { name: "joaozin" } }));
-  socket.send(JSON.stringify({ type: "hostGame" }));
-  socket.send(JSON.stringify({ type: "getGamelist" }));
+  // socket.send(JSON.stringify({ type: "createPlayer", data: { name: "joaozin" } }));
+  // socket.send(JSON.stringify({ type: "hostGame" }));
+  // socket.send(JSON.stringify({ type: "getGamelist" }));
 });
 
 socket.on("message", (data) => {
   console.log("Mensagem recebida do servidor:", data.toString());
+  try {
+    const msg = JSON.parse(data);
+    // if (msg.type == "gameCreated") {
+    //   socket.send(JSON.stringify({ type: "startGame", data: { id: msg.data.id } }));
+    // }
+  } catch (err) {
+    console.log("nao recebi um json rs");
+  }
 });
 
 socket.on("close", () => {
