c540f066b36cda05fe0e8e9cf91069a4285e9d8e
Renan Araujo Polainas <re.araujo.po@gmail.com>
2025-08-20 18:24:06 -0300

    safety checks

--- handlers/playerHandler.js
+++ handlers/playerHandler.js
@@ -1,6 +1,10 @@
 import { Player } from "../gameClasses/player.js";
 
 export function createPlayer(msg) {
+  // tem que ter um nome e so pode um jogador por conexao
+  if (!msg.data.name) return;
+  if (msg.players.find((p) => p.connection == msg.socket)) return;
+
   const player = new Player(msg.socket, msg.data.name);
   msg.players.push(player);
 
