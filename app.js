c344b343cc7096b09dbb8d7163553e1ce199a862
Renan Araujo Polainas <re.araujo.po@gmail.com>
2025-08-20 19:07:04 -0300

    aproveitando express para servir o dashboard

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ import express from "express";
 
 const app = express();
 app.use(express.json());
+//remover no deploy
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/client/utils/test.html");
+});
 
 export default app;
